faeaf110b295b68215c7b19f68c60644219abe4f
unknown <kniazev.artem.qa@gmail.com>
2025-12-01 01:23:13 -0500

    create test for tour

--- jest.config.js
+++ jest.config.js
@@ -5,7 +5,28 @@ const tsJestTransformCfg = createDefaultPreset().transform;
 /** @type {import("jest").Config} **/
 module.exports = {
   testEnvironment: "node",
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
   transform: {
     ...tsJestTransformCfg,
   },
+   reporters: [
+    "default",
+    [
+      "jest-junit",
+      {
+        outputDirectory: "test-reports",
+        outputName: "junit.xml",
+      },
+    ],
+    [
+      "jest-html-reporter",
+      {
+        pageTitle: "Test Report",
+        outputPath: "test-reports/test-report.html",
+        includeFailureMsg: true,
+        includeConsoleLog: true,
+      },
+    ],
+  ],
+
 };
